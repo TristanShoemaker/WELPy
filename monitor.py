cfedfc20e32b6217232e25c491e03ab9003e1f5d
Tristan <trstn.shmkr@gmail.com>
2020-03-14 20:59:41 -0400

    tweaks and now takes arguments for plot time

--- monitor.py
+++ monitor.py
@@ -1,16 +1,30 @@
 import WELServer
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import argparse
 
 dat = WELServer.WELData()
-timerange = [datetime.now() - timedelta(hours=24), 'none']
-fig, axes = plt.subplots(2, 1,
-                         sharex=True,
-                         figsize=(11,7),
-                         gridspec_kw={'height_ratios': [1, 0.7]})
 
-dat.plotVar('dateandtime',
-            ['gas_refrig_T',
+parser = argparse.ArgumentParser()
+parser.add_argument('hours', type=int, nargs=1,
+                    help='number of hours to plot')
+parser.add_argumetn('-a', action='store_true')
+args = parser.parse_args()
+
+if not args.a:
+    timerange = [datetime.now() - timedelta(hours=args.hours[0]), 'none']
+else:
+    timerange = ['none','none']
+    
+fig, axes = plt.subplots(3, 1,
+                         sharex=True,
+                         figsize=(12,8),
+                         gridspec_kw={'height_ratios': [1, 0.6, 0.6]})
+
+dat.plotVar([
+             'TAH_in_T',
+             'TAH_out_T',
+             'gas_refrig_T',
              'liqu_refrig_T',
              'loop_in_T',
              'loop_out_T',
@@ -20,12 +34,15 @@ dat.plotVar('dateandtime',
             timerange=timerange,
             axes=axes[0])
 
-# dat.plotVar('dateandtime',
-#             'eff_D',
-#             timerange=timerange,
-#             axes=axes[1])
-
 dat.plotStatus(timerange=timerange,
                axes=axes[1])
 
-fig.show()
+dat.plotVar(['eff_ma',
+            'eff_D'],
+            yunits='Efficiency W/C',
+            timerange=timerange,
+            axes=axes[2])
+
+
+
+plt.show()
