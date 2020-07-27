63d1ffa6d6b0d7ecc24e7031da8ea9d4375bcce9
Tristan <trstn.shmkr@gmail.com>
2020-07-27 19:23:04 -0400

    Changed standard plots

--- monitor.py
+++ monitor.py
@@ -1,49 +1,26 @@
 import WELServer
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-import argparse
-import sys
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-a', action='store_true',
-                    help='plot all data')
-parser.add_argument('-t', type=int, action='store',
-                    help='specify number of hours into past to '
-                         'plot. Example: <-t 12> plots the past 12 hours.')
-parser.add_argument('-r', type=str, action='store', nargs=2,
-                    help='specify start and end time to plot as two strings '
-                         'in iso format. Example: <-r \'2020-03-22 12:00\' '
-                         '\'2020-03-22 15:00\'>')
-
-args = parser.parse_args()
-
-if args.t:
-    timerange = [datetime.now() - timedelta(hours=args.t), 'none']
-if args.a:
-        timerange = ['none','none']
-if args.r:
-    print(args.r)
-    timerange = [datetime.fromisoformat(args.r[0]),
-                 datetime.fromisoformat(args.r[1])]
-if len(sys.argv) < 2:
-    timerange = [datetime.now() - timedelta(hours=12), 'none']
+import numpy as np
 
 
 dat = WELServer.WELData()
 
-fig, axes = plt.subplots(4, 1,
+timerange = dat.time_from_args()
+
+fig, axes = plt.subplots(3, 1,
                          sharex=True,
                          figsize=(12,9),
-                         gridspec_kw={'height_ratios': [1, 0.7, 0.7, 0.7]})
+                         gridspec_kw={'height_ratios': [1, 0.3, 0.3]})
+
 dat.plotVar(['TAH_in_T',
              'TAH_out_T',
-             'gas_refrig_T',
-             'liqu_refrig_T',
+             # 'gas_refrig_T',
+             # 'liqu_refrig_T',
              'loop_in_T',
              'loop_out_T',
              'outside_T',
              'living_T',
-             'wood_fire_T'],
+             'trist_T'],
             timerange=timerange,
             statusmask='heat_1_b',
             axes=axes[0])
@@ -51,19 +28,40 @@ dat.plotVar(['TAH_in_T',
 dat.plotStatus(timerange=timerange,
                axes=axes[1])
 
-dat.plotVar(['desup_T',
-             'desup_return_T',
-             'house_hot_T',
-             'buderus_h2o_T',
-             'tank_h2o_T'],
-             timerange=timerange,
-             axes=axes[2])
+full_range_delta = timerange[1] - timerange[0]
+rolling_interval = np.clip(round((full_range_delta.seconds / 3600) / 6), 1, 6)
+# rolling_interval = 6
+dat.plotVar([F"COP.rolling('{rolling_interval}H').mean()"],
+        timerange=timerange,
+        yunits=F'COP {rolling_interval} Hr Rolling Mean',
+        axes=axes[2])
 
-dat.plotVar(['eff_ma',
-            'eff_D'],
-            yunits='Efficiency W/C',
-            timerange=timerange,
-            axes=axes[3])
+# dat.plotVar(['TAH_fpm'],
+#             timerange=timerange,
+#             yunits='Wind Speed [m/s]',
+#             # statusmask='heat_1_b',
+#             axes=axes[2])
+
+# dat.plotVar(['desup_T',
+#              'desup_return_T',
+#              'house_hot_T',
+#              'buderus_h2o_T',
+#              'tank_h2o_T'],
+#              timerange=timerange,
+#              axes=axes[2])
+
+# dat.plotVar(['eff_ma',
+#             'eff_D'],
+#             yunits='Efficiency W/C',
+#             timerange=timerange,
+#             axes=axes[3])
+
+# cops = dat.plotVar(['COP',
+#                     'well_COP'],
+#             yunits='COP',
+#             timerange=timerange,
+#             statusmask='heat_1_b',
+#             axes=axes[3])
 
 
 plt.subplots_adjust(hspace=0.01)
